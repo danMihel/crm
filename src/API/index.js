13f05056d7f6da8a8d9eeb6f04af52ddd347cee6
danMihel <d.mihel78@gmail.com>
2022-09-01 14:32:19 +0300

    main

--- src/API/index.js
+++ src/API/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const loginConfig = {
-  baseURL: "http://192.168.0.254:8080/api/auth",
+  baseURL: "https://192.168.0.254:8080/api/auth",
 };
 
 export const LoginAPIInstanse = axios.create(loginConfig);
