5a01ecf72850b60c53786821768510e87719a5fd
danMihel <d.mihel78@gmail.com>
2022-09-05 13:11:50 +0300

    main

--- src/router/index.js
+++ src/router/index.js
@@ -30,9 +30,9 @@ const router = createRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
+/* router.beforeEach((to, from, next) => {
   if(to.name !== 'login' && store.state.AuthModule.logged === false) next( {name:'login'});
   else next()
-})
+}) */
 
 export default router
