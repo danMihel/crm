b51cafd0f667e2e2176784e677eebbe41236e5e5
danMihel <d.mihel78@gmail.com>
2022-09-08 16:06:05 +0300

    main

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,30 @@
 </template>
 
 <style>
-  @import '~bootstrap/dist/css/bootstrap.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   box-sizing: content-box;
+}
+h2{
+  text-align: center;
+}
+.wraper{
+  padding: 0 1rem;
+}
+.list-group {
+  margin: 1rem 0;
+}
+.list-group-item {
+  padding: 1rem;
+  list-style: none;
+  border: 1px solid rgb(219, 217, 217);
+  border-radius: 10px;
+}
+.list-group-item:hover{
+  background: #5e78ecbe;
+  color: azure;
+  transition: 0.4 s ease;
 }
 </style>
