c16d2483d0a8e854fbce9dad43367af678e122fb
danMihel <d.mihel78@gmail.com>
2022-09-20 12:38:22 +0300

    main

---
+++ src/components/CaseCards/CaseAddCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>Добавить компанию</p>
+    <input v-model="query" @input="findElement" type="text" />
+    <div v-for="item in searchedCompany" :key="item.id">
+      <div @click="addCompany(item)">{{ item.name }}</div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "case-add-card",
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    async addCompany(item) {
+      await this.$store.dispatch("CaseModule/postCompany", item.id);
+      await this.$store.dispatch(
+        "CaseModule/fetchIdCase",
+        this.$route.params.id
+      );
+      await this.$store.commit("CaseModule/setSearchedCompany", "");
+      this.query = "";
+    },
+    async findElement() {
+      await this.$store.commit("CaseModule/setSearchQuery", this.query);
+      await this.$store.dispatch("CaseModule/findCompany");
+    },
+  },
+  computed: {
+    ...mapState({
+      cases: (state) => state.CaseModule.case,
+      searchedCompany: (state) => state.CaseModule.searchedCompany,
+    }),
+  },
+};
+</script>
+<style></style>
