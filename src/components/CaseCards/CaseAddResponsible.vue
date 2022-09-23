d3a6a554467bc45aad5389f6b9ba71de4e368338
danMihel <d.mihel78@gmail.com>
2022-09-23 13:57:16 +0300

    main

--- src/components/CaseCards/CaseAddResponsible.vue
+++ src/components/CaseCards/CaseAddResponsible.vue
@@ -3,16 +3,16 @@
   <div v-if="!visible" @click="visible = !visible">Добавить</div>
   <div v-if="visible">
     <div @click=" closeSearchInput">Закрыть</div>
-    <input v-model="query" @input="findPerson" type="text" class="add-person-input"/>
-    <div v-for="item in searchedPerson" :key="item.id" class="add-person-output">
-      <div class="add-person-output-item" @click="addPerson(item)">{{ item.name }}</div>
+    <input v-model="query" @input="findResponsible" type="text" class="add-responsible-input"/>
+    <div v-for="item in searchedResponsible" :key="item.id" class="add-responsible-output">
+      <div class="add-responsible-output-item" @click="addResponsible(item)">{{ item.name }}</div>
     </div>
   </div>
 </template>
 <script>
 import { mapState } from "vuex";
 export default {
-  name: "case-add-person-card",
+  name: "case-add-responsible-card",
   data() {
     return {
       visible: false,
@@ -20,47 +20,47 @@ export default {
     };
   },
   methods: {
-    async addPerson(item) {
-      console.log('addPErson')
-      await this.$store.dispatch("CaseModule/postPerson", item.id);
+    async addResponsible(item) {
+      console.log('addresponsible')
+      await this.$store.dispatch("CaseModule/postResponsible", item.id);
       await this.$store.dispatch("CaseModule/fetchIdCase", this.$route.params.id);
-      await this.$store.commit("CaseModule/setSearchedPerson", "");
+      await this.$store.commit("CaseModule/setSearchedResponsible", "");
       this.query = "";
       this.visible = false;
     },
-    async findPerson() {
-      console.log('find PErson')
+    async findResponsible() {
+      console.log('find responsible')
       await this.$store.commit("CaseModule/setSearchQuery", this.query);
-      await this.$store.dispatch("CaseModule/findPerson",);
+      await this.$store.dispatch("CaseModule/findResponsible",);
     },
     closeSearchInput(){
       this.query = "";
       this.visible = false;
-      this.$store.commit("CaseModule/setSearchedPerson", "");
+      this.$store.commit("CaseModule/setSearchedResponsible", "");
     }
   },
   computed: {
     ...mapState({
       cases: (state) => state.CaseModule.case,
-      searchedPerson: (state) => state.CaseModule.searchedPerson,
+      searchedresponsible: (state) => state.CaseModule.searchedresponsible,
     }),
   },
 };
 </script>
 <style>
-  .add-person-output{
+  .add-responsible-output{
     border-right: 1px solid black;
     border-left: 1px solid black;
     width: 200px;
   }
-  .add-person-output:last-child{
+  .add-responsible-output:last-child{
     border-bottom: 1px solid black;
   }
-  .add-person-output-item:hover{
+  .add-responsible-output-item:hover{
     background-color: #5e78ecbe;
     cursor: pointer;
   }
-  .add-person-input{
+  .add-responsible-input{
     width: 200px;
   }
 </style>
